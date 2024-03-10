55cab84a7a900bee39fbdcaf42e184b6e9184184
piraxis2 <kate107852@gmail.com>
2024-03-10 21:31:01 +0900

    11723번 돌 게임

---
+++ 11723.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    set<int> setter;
+    setter.emplace(1);
+    setter.emplace(3);
+    for (int i = 4; i <= N; i++)
+    {
+        if (!(setter.count(i - 1) || setter.count(i - 3)))
+        {
+            setter.emplace(i);
+        }
+    }
+    cout << (setter.count(N) ? "SK" : "CY");
+}
