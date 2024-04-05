08bb80a03ddec59b1b26c258914ba638a36fccc3
piraxis2 <kate107852@gmail.com>
2024-04-05 16:44:05 +0900

    11723 집합(비트마스킹)

---
+++ 11723.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int bitmask = 0;
+    constexpr int allbit = (1 << 20) - 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        string Command;
+        cin >> Command;
+        if (Command == "empty")
+        {
+            bitmask = 0;
+            continue;
+        }
+        if(Command == "all")
+        {
+            bitmask = allbit;
+            continue;
+        }
+
+        int value;
+        cin >> value;
+        const int bitTarget = (1 << (value - 1));
+        if (Command == "add")
+        {
+            bitmask |= bitTarget;
+        }
+        if (Command == "remove")
+        {
+            bitmask &= (bitTarget ^ allbit);
+        }
+        if(Command == "check")
+        {
+            cout << ((bitmask & bitTarget) > 0 ? 1 : 0) << endl;
+        }
+        if (Command == "toggle")
+        {
+            if ((bitmask & bitTarget) > 0)
+            {
+                bitmask &= (bitTarget ^ allbit);
+            }
+            else
+            {
+                bitmask |= bitTarget;
+            }
+        }
+    }
+}
