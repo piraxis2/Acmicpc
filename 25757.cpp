87751e93ba81a27b9fa0320f6655849077f3fc6e
piraxis2 <kate107852@gmail.com>
2024-04-09 15:04:04 +0900

    25757번 임스와 함께하는 미니게임

---
+++ 25757.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <set>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n;
+    char c;
+    cin >> n >> c;
+    set<string> palyers;
+    for (int i = 0; i < n; i++)
+    {
+        string input;
+        cin >> input;
+        palyers.emplace(input);
+    }
+    switch (c)
+    {
+    case 'Y':
+        cout << palyers.size()/1;
+        break;
+    case 'F':
+        cout << palyers.size()/2;
+        break;
+    case 'O':
+        cout << palyers.size()/3;
+        break;
+    }
+}
